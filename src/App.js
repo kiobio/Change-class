7297c97ed9d4af5dbb3d042a92a8dd6fc2ffcf57
unknown <aneta.fojtikova95@gmail.com>
2021-04-14 14:33:52 +0200

    toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[blue, setBlue]= useState(true)
+  console.log(blue)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Body">
+      <div className={blue ? "square blue" : "square green"}
+      onClick={ () => setBlue(!blue) }
+      >
+        {blue ? "Blue" : "Green"}
+      </div>
     </div>
   );
 }
